898a4bc131246f52e4dac1a4643a2f211e63b826
godswillJohn <godswillodudu21@gmail.com>
2023-11-11 14:17:32 +0100

    You need to be qick in order to answer all questions on time

---
+++ question-bank.js
@@ -0,0 +1,93 @@
+const questions = [
+    {
+    question: "When did Nigeria get their independence",
+    options: {
+        A: "1975",
+        B: "1963",
+        C: "1960",
+        D: "1958"
+    },
+    correctAnswer: "1960"
+},
+{
+    question: "Which country colonized Africa the Most",
+    options: {
+        A: "Portugal",
+        B: "England",
+        C: "France",
+        D: "Britain"
+    },
+    correctAnswer: "Britain"
+},
+{
+    question: "How many states are in Nigeria",
+    options: {
+        A: "36",
+        B: "41",
+        C: "117 ",
+        D: "306"
+    },
+    correctAnswer: "36"
+},
+
+{
+    question: "Who is the current president of Nigeria",
+    options: {
+        A: "Sir Ahmado Bello",
+        B: "Mr Enofiok Luke",
+        C: "Sir Abubaka Tafawa ",
+        D: "Mr Asiwaju Tinubu"
+    },
+    correctAnswer: "Mr Asiwaju Tinubu"
+},
+{
+    question: "What's the full meaning of EFCC",
+    options: {
+        A: "Economic Financial commition co-operation",
+        B: "Economic Federal constitution campaign",
+        C: "Economic Financial Crime Commission",
+        D: "Enhance and Financial Crime co-operation"
+    },
+    correctAnswer: "Economic and Financial Crime Commission"
+},
+{
+question: "What is the capital of Nigeria",
+    options: {
+        A: "Europe",
+        B: "Lagos",
+        C: "Abuja",
+        D: "Ghana"
+    },
+    correctAnswer: "Abuja"
+},
+{
+question: "Nigeria is situated in the Western and has a coastal border on which body of water",
+    options: {
+        A: "Mozambique channel",
+        B: "Indian Ocean",
+        C: "Gulf of Guinea",
+        D: "Mediterran sea"
+    },
+    correctAnswer: "Gulf of Guinea"
+},
+{
+question: "What is the capital of Kwara state",
+    options: {
+        A: "Lokoja",
+        B: "Abia",
+        C: "Koji",
+        D: "Ilorin"
+    },
+    correctAnswer: "Ilorin"
+},
+{
+question: "Which state in Nigeria is one of the largest by area",
+    options: {
+        A: "Niger state",
+        B: "Adamawa state",
+        C: "Zamfara state",
+        D: "Kwara state"
+    },
+    correctAnswer: "Niger state"
+}
+]
